793020d0dcd02ab0de01cef250ca9b4cdcc476ba
huukhanh1369 <trnghuukhanh2408@gmail.com>
2025-01-07 23:34:22 +0700

    Add files via upload

--- SESSION16-BT3.c
+++ SESSION16-BT3.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
-int sum( int *a, int *b)
+void sum( int *a, int *b,int *tong)
 {
-    return *a + *b;
+    *tong = *a + *b;
 }
 int main()
 {
-    int x= 5, y= 10;
-    int result = sum(&x,&y);
-    printf("tong cua 2 bien la : %d",result);
+    int x= 5, y= 10, tong ;
+    sum(&x,&y,&tong);
+    printf("tong cua 2 bien la : %d",tong);
     return 0;
 }
--- SESSION16-BT4.c
+++ SESSION16-BT4.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
-void printArray(int *arr, int size)
+void printArray(int *arr)
 {
-    for( int i = 0; i < size; i++ )
+    for( int i = 0; i < 9; i++ )
     {
         printf("array[%d] : %d\n",i,*(arr+i));
     }
@@ -10,6 +10,6 @@ int main()
 {
     int array[]={1,2,3,4,5,6,7,8,9};
     int size = sizeof array / sizeof(int);
-    printArray(array,size);
+    printArray(array);
     return 0;
 }
